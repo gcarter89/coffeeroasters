e50a8c725531c899f0f14cb778e3cc7469cf95ec
Gareth Carter <gareth.carter@outlook.com>
2022-05-15 23:15:48 +0100

    completed header styling for tablet

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -1,4 +1,5 @@
 import Hamburger from "./Hamburger";
+import styles from './Nav.module.scss';
 
 export default function Nav(props: {
     hamburgerOpen:boolean
@@ -8,7 +9,14 @@ export default function Nav(props: {
 ) {
     return (
         <nav>
-            <Hamburger hamburgerOpen={props.hamburgerOpen} handleHamburgerClick={props.handleHamburgerClick} />
+            <div className={styles.nav__hamburger}>
+                <Hamburger hamburgerOpen={props.hamburgerOpen} handleHamburgerClick={props.handleHamburgerClick} />
+            </div>
+            <div className={styles.nav__links}>
+                <a onClick={(e) => e.preventDefault()} href="/">HOME</a>
+                <a onClick={(e) => e.preventDefault()} href="/about">ABOUT US</a>
+                <a onClick={(e) => e.preventDefault()} href="/plan">CREATE YOUR PLAN</a>
+            </div>
         </nav>
         
         
