b91be37810e346ca56926a3349aead9aec81d351
Gareth Carter <gareth.carter@outlook.com>
2022-05-16 23:57:46 +0100

    desktop sizing for collection styling complete

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -22,36 +22,37 @@ export default function Home() {
                 <div className={styles.home__collectionTitle}>
                     <h1>our collection</h1>
                 </div>
-
-                <div className={styles.home__collectionCard}>
-                    <img src={GranEspresso} alt="Gran Espresso coffee beans" />
-                    <div className={styles.home__collectionCardText}>
-                        <h4>Gran Espresso</h4>
-                        <p>Light and flavorful blend with cocoa and black pepper for an intense experience</p>
+                <div className={styles.home__collectionCards}>
+                    <div className={styles.home__collectionCard}>
+                        <img src={GranEspresso} alt="Gran Espresso coffee beans" />
+                        <div className={styles.home__collectionCardText}>
+                            <h4>Gran Espresso</h4>
+                            <p>Light and flavorful blend with cocoa and black pepper for an intense experience</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.home__collectionCard}>
-                    <img src={Planalto} alt="Planalto coffee beans" />
-                    <div className={styles.home__collectionCardText}>
-                        <h4>Planalto</h4>
-                        <p>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+                    <div className={styles.home__collectionCard}>
+                        <img src={Planalto} alt="Planalto coffee beans" />
+                        <div className={styles.home__collectionCardText}>
+                            <h4>Planalto</h4>
+                            <p>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.home__collectionCard}>
-                    <img src={Piccollo} alt="Piccollo coffee beans" />
-                    <div className={styles.home__collectionCardText}>
-                        <h4>Piccollo</h4>
-                        <p>Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
+                    <div className={styles.home__collectionCard}>
+                        <img src={Piccollo} alt="Piccollo coffee beans" />
+                        <div className={styles.home__collectionCardText}>
+                            <h4>Piccollo</h4>
+                            <p>Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.home__collectionCard}>
-                    <img src={Danche} alt="Danche coffee beans" />
-                    <div className={styles.home__collectionCardText}>
-                        <h4>Danche</h4>
-                        <p>Ethiopian hand-harvested lend densely packed with vibrant fruit notes</p>
+                    <div className={styles.home__collectionCard}>
+                        <img src={Danche} alt="Danche coffee beans" />
+                        <div className={styles.home__collectionCardText}>
+                            <h4>Danche</h4>
+                            <p>Ethiopian hand-harvested lend densely packed with vibrant fruit notes</p>
+                        </div>
                     </div>
                 </div>
             </section>
