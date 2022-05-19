9705ee802dd4f2eb0abbe3787f028a9ec1fabf88
Gareth Carter <gareth.carter@outlook.com>
2022-05-19 01:03:43 +0100

    about mobile layout completed

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,6 +2,10 @@ import styles from './About.module.scss';
 import commitmentMobileImage from '../../assets/about/mobile/image-commitment.jpg';
 import qualityMobileImage from '../../assets/about/mobile/image-quality.jpg';
 
+import { ReactComponent as UnitedKingdom } from '../../assets/about/desktop/illustration-uk.svg';
+import { ReactComponent as Australia } from '../../assets/about/desktop/illustration-australia.svg';
+import { ReactComponent as Canada } from '../../assets/about/desktop/illustration-canada.svg';
+
 export default function About() {
     return (
         <>
@@ -29,7 +33,36 @@ export default function About() {
             </div>
         </section>
 
-        <section className={styles.about__headquarters}></section>
+        <section className={styles.about__headquarters}>
+            <h4>Our headquarters</h4>
+
+            <div className={styles.about__locationCards}>
+                <div className={styles.about__locationCard}>
+                    <UnitedKingdom />
+                    <h2>United Kingdom</h2>
+                    <p>68  Asfordby Rd</p>
+                    <p>Alcaston</p>
+                    <p>SY6 1YA</p>
+                    <p>+44 1241 918425</p>
+                </div>
+                <div className={styles.about__locationCard}>
+                    <Canada />
+                    <h2>Canada</h2>
+                    <p>1528  Eglinton Avenue</p>
+                    <p>Toronto</p>
+                    <p>Ontario M4P 1A6</p>
+                    <p>+1 416 485 2997</p>
+                </div>
+                <div className={styles.about__locationCard}>
+                    <Australia />
+                    <h2>Australia</h2>
+                    <p>36 Swanston Street</p>
+                    <p>Kewell</p>
+                    <p>Victoria</p>
+                    <p>+61 4 9928 3629</p>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
