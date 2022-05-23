415df42d6d6ca4d79b23849485973072b89cb2ac
Gareth Carter <gareth.carter@outlook.com>
2022-05-23 01:04:37 +0100

    initial plan hero tablet styling

---
+++ src/components/plan/Plan.tsx
@@ -0,0 +1,26 @@
+import styles from './Plan.module.scss';
+
+export default function Plan() {
+    return (
+        <>
+            <section className={styles.plan__hero}>
+                <div className={styles.plan__heroText}>
+                    <h1>Create a plan</h1>
+                    <p>Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.</p>
+                </div>
+            </section>
+
+            <section>
+
+            </section>
+
+            <section>
+
+            </section>
+
+            <section>
+
+            </section>
+        </>
+    )
+}
