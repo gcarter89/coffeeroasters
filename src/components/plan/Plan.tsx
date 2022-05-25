f9fc5ae5098d941c88bc9a0fcced1dadfd56c80d
Gareth Carter <gareth.carter@outlook.com>
2022-05-25 01:30:09 +0100

    steps mobile layout complete

--- src/components/plan/Plan.tsx
+++ src/components/plan/Plan.tsx
@@ -10,8 +10,26 @@ export default function Plan() {
                 </div>
             </section>
 
-            <section>
+            <section className={styles.plan__steps}>
+                <div className={styles.plan__stepsContainer}>
+                    <div className={styles.plan__stepsCard}>
+                        <span>01</span>
+                        <h2>Pick your coffee</h2>
+                        <p>Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.</p>
+                    </div>
+                    <div className={styles.plan__stepsCard}>
+                        <span>02</span>
+                        <h2>Choose the frequency</h2>
+                        <p>Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.</p>
 
+
+                    </div>
+                    <div className={styles.plan__stepsCard}>
+                        <span>03</span>
+                        <h2>Receive and enjoy!</h2>
+                        <p>We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.</p>
+                    </div>
+                </div>
             </section>
 
             <section>
