92ea3ac494f4a8627debe6e1a31afcf2c4156998
Gareth Carter <gareth.carter@outlook.com>
2022-06-05 20:58:33 +0100

    added hover state for button with basic nav features

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/header/Header';
-// import Home from './components/home/Home';
-// import About from './components/about/About';
+import Home from './components/home/Home';
+import About from './components/about/About';
 import Plan from './components/plan/Plan';
 import Footer from './components/footer/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
@@ -19,9 +20,11 @@ function App() {
       <>
         <Header handleHamburgerClick={handleHamburgerClick} hamburgerOpen={hamburgerOpen} />
         <main>
-            {/* <Home /> */}
-            {/* <About /> */}
-            <Plan />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="plan" element={<Plan />} />
+                <Route path="about" element={<About />} />
+            </Routes>            
         </main>
         <Footer />
       </>
